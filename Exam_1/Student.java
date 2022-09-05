40b84f6c3f02f8389d09d75bee3b8d158844771d
Mark simmone delos reyes <111473154+Simmoneee@users.noreply.github.com>
2022-09-05 10:11:54 +0800

    Add files via upload

---
+++ Exam_1/Student.java
@@ -0,0 +1,26 @@
+public class Student {
+   
+   String surname;
+   String firstname;
+   char middleInitial;
+   int dateOfBirth;
+   int studentNumber;
+   String studentEmailAddress;
+   boolean iAmAwesome;
+   
+
+
+    public void sayMyStudentNumber() {
+        System.out.println("Hello my Student Number is" + studentNumber);
+        }
+
+    public void sayMyEmailAddress(){
+        System.out.println("Hello my Email Address is" + studentEmailAddress);
+    }
+    public void sayamIAwesome() {
+        System.out.println("Hello I Am Awesome" + iAmAwesome);
+        } 
+
+     
+         
+    }
